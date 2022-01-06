3a86353ee12a39cc17260ec9340f14cc27ab426b
Gilberto <Betho6990@gmail.com>
2022-01-06 17:22:49 -0600

    Request y Response

--- Curso de Flask/platzi-flask/main.py
+++ Curso de Flask/platzi-flask/main.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return 'Hello World Flask'
+    user_ip = request.remote_addr
+    return 'Hello World Platzis, tu IP es {}'.format(user_ip)
