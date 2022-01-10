b0a03c67ccd72600c5e2e573b90f5486d4a6d6c3
Gilberto <Betho6990@gmail.com>
2022-01-09 18:40:51 -0600

    Using POST method in Flask-WTF

--- Curso de Flask/platzi-flask/main.py
+++ Curso de Flask/platzi-flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap 
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
@@ -34,15 +34,27 @@ def index():
 
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     user_ip = session.get('user_ip')
     login_form = LoginForm()
+    username = session.get('username')
     context = {
         'user_ip': user_ip,
         'todos': todos,
-        'login_form': login_form
+        'login_form': login_form,
+        'username': username
     }
+    
+    # Esta ruta va a detectar cuando estás mandando un POST y va a validar la forma,
+    # De esta forma GET es el template, POST redirect a inicio.html
+    if login_form.validate_on_submit():
+        # Si es válido obtenemos el username
+        username = login_form.username.data
+        # Vamos a guardar este username en la sesión
+        session['username'] = username
+        
+        return redirect(url_for('index'))
 
     # Expandir variables automáticamente
     return render_template('hello.html', **context)
