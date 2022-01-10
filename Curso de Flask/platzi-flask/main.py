f68bd78dce0ba93ac2e9b8f314c05151dd9ba915
Gilberto <Betho6990@gmail.com>
2022-01-09 18:05:42 -0600

    Flask-Bootstrap and Flask-WTF implements

--- Curso de Flask/platzi-flask/main.py
+++ Curso de Flask/platzi-flask/main.py
@@ -1,10 +1,24 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap 
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+# Esto viene a querer encriptar el IP del usuario y usar "sesion()" function
+# Es un diccionario que tiene llaves y valores
+# En lugar de guardarlo en la cookie
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video a productor']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -16,17 +30,19 @@ def index():
     user_ip = request.remote_addr
     
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': login_form
     }
 
-    #Expandir variables automáticamente
+    # Expandir variables automáticamente
     return render_template('hello.html', **context)
---
+++ Curso de Flask/platzi-flask/script.sh
@@ -0,0 +1,5 @@
+source .env/bin/activate.fish
+export FLASK_APP=main.py
+export FLASK_DEBUG=1
+export FLASK_ENV=development
+flask run
