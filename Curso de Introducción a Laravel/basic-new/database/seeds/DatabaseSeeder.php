be5c0de2fa0bf753502e53ed5e04c9b5633bb7dd
Gilberto <betho6990@gmail.com>
2022-07-19 23:08:28 -0500

    Initial commit of Third Section of Introduction to Laravel Course

--- Curso de Introducción a Laravel/basic-new/database/seeds/DatabaseSeeder.php
+++ Curso de Introducción a Laravel/basic-new/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+
+        App\User::create([
+            'name' => 'Italo Morales F',
+            'email' => 'i@admin.com',
+            'password' => bycrypt('123456')
+        ]);
+
+        factory(App\Post::class, 24)->create();
     }
 }
