0c465321cd361a1e3bab5d0612ac16b35f66fc2c
Gilberto <betho6990@gmail.com>
2022-08-18 23:13:31 -0500

    Livewire: component design

--- Curso de Introducción a Laravel 8/j-livewire-8/app/Http/Livewire/CourseList.php
+++ Curso de Introducción a Laravel 8/j-livewire-8/app/Http/Livewire/CourseList.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Course;
 use Livewire\Component;
 
 class CourseList extends Component
 {
     public function render()
     {
-        return view('livewire.course-list');
+        return view('livewire.course-list', [
+            'courses' => Course::latest()->with('user')->take(9)->get()
+        ]);
     }
 }
