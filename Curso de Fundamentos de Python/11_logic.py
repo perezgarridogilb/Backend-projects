f169d5a7bb37ba51ccca647b01d28a45966578f1
Gilberto <Betho6990@gmail.com>
2022-11-19 23:59:30 -0600

    Logic Operator And and Of of Python Fundaments Course

---
+++ Curso de Fundamentos de Python/11_logic.py
@@ -0,0 +1,15 @@
+
+print(not True)
+print(not False)
+
+# and
+print('NOT AND')
+print('True and True =>', not (True and True))
+print('True and False =>', not (True and False))
+print('True and False =>', not (True and False))
+print('True and False =>', not (True and False))
+
+stock = input('Ingrese el número de stock => ')
+stock = int(stock)
+
+print(not (stock >= 100 and stock <= 1000))
