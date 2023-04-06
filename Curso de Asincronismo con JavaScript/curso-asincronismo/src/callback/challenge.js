f40c334f54053488db1aebd93beaf5acc6f57d10
Gilberto <Betho6990@gmail.com>
2023-04-05 23:48:55 -0600

    Fetch POST of Asynchronism with JavaScript Course

--- Curso de Asincronismo con JavaScript/curso-asincronismo/src/callback/challenge.js
+++ Curso de Asincronismo con JavaScript/curso-asincronismo/src/callback/challenge.js
@@ -1,7 +1,7 @@
 /**
  * Biblioteca que previamente instalamos
  */
-const XMLHttpRequest = require('xmlhttprequest');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 /**
  * Root de nuestra API
  */
@@ -23,19 +23,27 @@ function fetchData(urlApi, callback){
              * 3 → Procesamiento si existe alguna descarga.
              * 4 → Completado.
              */
-            if(xhttp.status === 200) {
+            if (xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
-            }
-        } else {
+            } else {
             const error = new Error('Error' + urlApi);
             return callback(error, null);
-        }
+            }
+        } 
     }
     xhttp.send();
 }
 
 fetchData(`${API}/products`, function (error1, data1) {
-    if (error1) return console.error(error1)
-})
-
+    if (error1) return console.error(error1);
+    fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) {
+        if (error2) return console.error(error2);
+        fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3) {
+            if (error3) return console.error(error3);
+            console.log(data1[0]);
+            console.log(data2.title);
+            console.log(data3.name);
+    });
+});
+});
 
