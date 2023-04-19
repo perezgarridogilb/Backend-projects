9e74f6720d8561129874bb618f2c60ba95183af5
Gilberto <Betho6990@gmail.com>
2023-04-19 17:38:53 -0600

    ES6:generator of ECMAScript Course: History and Versions of JavaScript

--- Curso de ECMAScript: Historia y Versiones de JavaScript/curso-emascript/es6/07-clases.js
+++ Curso de ECMAScript: Historia y Versiones de JavaScript/curso-emascript/es6/07-clases.js
@@ -1,33 +1,72 @@
+
+// declarando
+class User {};
+// instancia de una clase
+// const newUser = new User();
+
+class user {
+    // métodos
+    greeting() {
+        return 'Hello';
+    }
+}
+
+const gndx = new user();
+console.log(gndx.greeting());
+const bebedeoscar = new user();
+console.log(bebedeoscar.greeting());
+
+// constructor
+
+class user {
+    constructor() {
+        console.log('Nuevo Usuario');
+    }
+    greeting() {
+        return 'Hello';
+    }
+}
+
+const david = new user();
+
+// this
+
 class user {
     constructor(name) {
         this.name = name;
     }
-
+    // métodos
     speak() {
         return 'Hello';
     }
-
     greeting() {
         return `${this.speak()} ${this.name}`;
     }
 }
 
+const ana1 = new user('Ana');
+console.log(ana1.greeting());
+
+// setters and getters
+
 const ana = new user('Ana');
 console.log(ana.greeting());
 
+
 class user {
+    // constructor
     constructor(name, age) {
         this.name = name;
         this.age = age;
     }
-
+    // metodos
     speak() {
         return 'Hello';
     }
     greeting() {
         return `${this.speak()} ${this.name}`;
     }
-
+    
     get uAge() {
         return this.age;
     }
@@ -35,5 +74,10 @@ class user {
     set uAge(u) {
         this.age = n;
     }
-
 }
+
+const bebeloper1 = new user('David', 15);
+console.log(bebeloper1.uAge);
+console.log(bebeloper1.uAge = 20);
+
+
---
+++ Curso de ECMAScript: Historia y Versiones de JavaScript/curso-emascript/es6/es1.js
@@ -0,0 +1,5 @@
+import getData from "./api.js";
+export function solution() {
+  // Tu código aquí 👈
+  return getData();
+}
